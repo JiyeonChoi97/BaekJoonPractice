61264a0aa740c345ac6d0589a6142f21d4c79878
JiyeonChoi97 <57701500+JiyeonChoi97@users.noreply.github.com>
2020-02-27 01:49:27 +0900

    Math2

---
+++ BaekJun/src/Lv9_Math2/Q4153.java
@@ -0,0 +1,24 @@
+package Lv9_Math2;
+
+import java.util.Scanner;
+
+public class Q4153 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		while(true){
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			
+			if(a==0&&b==0&&c==0) break;
+			if(c*c == a*a + b*b) System.out.println("right");
+			else if(a*a == c*c + b*b) System.out.println("right");
+			else if(b*b == c*c + a*a) System.out.println("right");
+			else System.out.println("wrong");
+		}
+	}
+
+}
