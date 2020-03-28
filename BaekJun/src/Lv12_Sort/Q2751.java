49d5bc9efe9230362342f48d89a77bb08054f901
JiyeonChoi97 <57701500+JiyeonChoi97@users.noreply.github.com>
2020-03-29 03:16:58 +0900

    Lv12_Sort)N개의 수 오름차순 정렬2

--- BaekJun/src/Lv12_Sort/Q2751.java
+++ BaekJun/src/Lv12_Sort/Q2751.java
@@ -2,27 +2,24 @@
 package Lv12_Sort;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Q2751 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		try {
-			int n = Integer.parseInt(br.readLine());
-			ArrayList<Integer> list = new ArrayList<Integer>();
-			for (int i = 0; i < n; i++) {
-				list.add(Integer.parseInt(br.readLine()));
-			}
-			Collections.sort(list);
-			for (int i = 0; i < n; i++) {
-				System.out.println(list.get(i));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
+		int n = Integer.parseInt(br.readLine());
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < n; i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+		Collections.sort(list);
+		for (int i = 0; i < n; i++) {
+			System.out.println(list.get(i));
 		}
 	}
 
